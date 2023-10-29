e8feba0f738a95d7260469a06f887743a2891227
juandiego9221 <juandiego9221@gmail.com>
2023-10-28 21:18:35 -0500

    test de get all categories

--- src/test/java/pe/com/jdmm21/controller/CategoryControllerTest.java
+++ src/test/java/pe/com/jdmm21/controller/CategoryControllerTest.java
@@ -5,16 +5,22 @@ import static io.restassured.RestAssured.given;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.module.jsv.JsonSchemaValidator;
 
 @QuarkusTest
 public class CategoryControllerTest {
 
     @Test
     void getAllCategories() {
+        // add a json schema validator
+        JsonSchemaValidator validator = JsonSchemaValidator
+                .matchesJsonSchemaInClasspath("schemas/categoriesList.json");
+
         given()
-            .when().get("/categories")
-            .then()
-            .statusCode(200);
+                .when().get("/categories")
+                .then()
+                .statusCode(200)
+                .body(validator);
     }
-    
+
 }
